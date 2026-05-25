7a038892807565f50921951b349f29c6fa37a9bf
Ahmedsami2hvuv <reozaki.1211@gmail.com>
2026-05-25 19:41:19 +0300

    نخنخ

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,1 +1,4 @@
+// Prevent 'message' event handler warning by adding it in the initial evaluation
+self.addEventListener('message', () => { });
+
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
