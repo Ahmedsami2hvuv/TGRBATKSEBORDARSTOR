d74cb0d184ef87e2a7c3ef91f9c1bcd075e56ed2
Ahmedsami2hvuv <reozaki.1211@gmail.com>
2026-06-16 01:57:41 +0300

    إنشاء مفتاح التوقيع وتكوين تطبيق المندوب كنسخة Release لتقليل تحذيرات جوجل (Play Protect)

--- mandob-app/app/build.gradle.kts
+++ mandob-app/app/build.gradle.kts
@@ -17,6 +17,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("mandob-release-key.jks")
+            storePassword = "mandob123"
+            keyAlias = "mandob"
+            keyPassword = "mandob123"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -24,6 +33,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
